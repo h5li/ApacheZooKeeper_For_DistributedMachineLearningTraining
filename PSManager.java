613bfe4e1364038b67fd8f58ff278e1847192b8d
Rishikesh Ingale <rishikesh.ingale@gmail.com>
2021-05-28 17:53:10 -0700

    Finished PS; will change depending on tests of course

--- PSManager.java
+++ PSManager.java
@@ -2,52 +2,77 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.KeeperException.Code;
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.AsyncCallback.DataCallback;
+import org.apache.zookeeper.data.Stat;
 
-class PSManager {
+class PSManager implements DataCallback {
+    public float[][] grid;
+    public int numParams;
+    public int numWorkers;
+    public int numDone;
+
+    public PSManager(int numWorkers) {
+        this.grid = null;
+        this.numParams = 0;
+        this.numWorkers = numWorkers;
+        this.numDone = 0;
+    }
+
     public static void main(String[] args) throws KeeperException, InterruptedException, IOException {
-        if (args.length < 1) {
-            System.err.println("USAGE: PSManager [host:port ...]");
+        if (args.length < 2) {
+            System.err.println("USAGE: PSManager numWorkers [host:port ...]");
             System.exit(2);
         }
 
-        String addrs = args[0];
-        for (int i = 1; i < args.length; i++)
+        int numWorkers = Integer.parseInt(args[0]);
+        String addrs = args[1];
+        for (int i = 2; i < args.length; i++)
             addrs += "," + args[i];
         ZooKeeper zk = new ZooKeeper(addrs, 3000, null);
+        PSManager manager = new PSManager(numWorkers);
 
         // 1. create the znodes
-        int done = 0;
-        for (String arg : args)
-            zk.create("/w", null, null, CreateMode.EPHEMERAL_SEQUENTIAL);
-        zk.create("/m", null, null, CreateMode.PERSISTENT);
-
-        // 2. get the data once training is completed
-        float[][] grid = null;
-        int numParams = 0;
-        for (int i = 0; i < args.length; i++) {
-            byte[] params = zk.getData("/w" + i, true, null);
-            if (grid == null) {
-                numParams = params.length / 4;
-                grid = new float[args.length][numParams];
-            }
-            for (int j = 0; j < params.length; i += 4) {
-                grid[i][j] = ByteBuffer.wrap(params, i, 4).getFloat();
-            }
+        if (zk.exists("/m", false) == null)
+            zk.create("/m", null, null, CreateMode.PERSISTENT);
+        for (int i = 0; i < numWorkers; i++) {
+            String path = "/w" + i;
+            zk.create(path, null, null, CreateMode.EPHEMERAL);
+            zk.getData(path, true, manager, null);
         }
+        zk.create("/start", null, null, CreateMode.EPHEMERAL);
 
-        // 3. write the average to the znode
-        byte[] vector = new byte[numParams * 4];
-        for (int i = 0; i < numParams; i++) {
+        // 2. wait for training to be completed
+        while (manager.numDone != numWorkers);
+
+        // 3. write the average to the manager znode
+        byte[] vector = new byte[manager.numParams * 4];
+        for (int i = 0; i < manager.numParams; i++) {
             float sum = 0;
-            for (int j = 0; j < args.length; j++)
-                sum += grid[j][i];
-            float val = sum / args.length;
+            for (int j = 0; j < numWorkers; j++)
+                sum += manager.grid[j][i];
+            float val = sum / numWorkers;
 
             byte[] byteRep = ByteBuffer.allocate(4).putFloat(val).array();
             for (int j = 0; j < 4; j++)
                 vector[4 * i + j] = byteRep[j];
         }
         zk.setData("/m", vector, -1);
+    }
+
+    public void processResult(int rc, String path, Object ctx, byte[] data, Stat stat) {
+        if (rc != Code.OK.intValue()) {
+            System.out.println("Something's wrong");
+            return;
+        }
+
+        if (this.grid == null) {
+            this.numParams = data.length / 4;
+            this.grid = new float[this.numWorkers][this.numParams];
+        }
+        for (int j = 0; j < this.numParams; j++)
+            this.grid[this.numDone][j] = ByteBuffer.wrap(data, 4 * j, 4).getFloat();
+        this.numDone++;
     }
 }
--- PSWorker.java
+++ PSWorker.java
@@ -1,9 +1,18 @@
-import java.io.IOException;
+import java.io.*;
 import java.nio.ByteBuffer;
+import java.util.*;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.KeeperException.Code;
+import org.apache.zookeeper.AsyncCallback.DataCallback;
+import org.apache.zookeeper.data.Stat;
 
-class PSWorker {
+class PSWorker implements DataCallback {
+    public float[] vector;
+
+    public PSWorker() {
+        this.vector = null;
+    }
     public static void main(String[] args) throws KeeperException, InterruptedException, IOException {
         if (args.length < 2) {
             System.err.println("USAGE: PSWorker workerNumber [host:port ...]");
@@ -15,10 +24,49 @@ class PSWorker {
         for (int i = 2; i < args.length; i++)
             addrs += "," + args[i];
         ZooKeeper zk = new ZooKeeper(addrs, 3000, null);
+        PSWorker worker = new PSWorker();
+        while(zk.exists("/start", false) == null);
+        zk.getData("/m", true, worker, null);
 
         // 1. Execute training script
-        // 2. Read params and convert to byte array
-        // 3. Write the data to znode with same workerId
-        // 4. Get the average gradient from the manager znode
+        ProcessBuilder pb = new ProcessBuilder("python", "train.py");
+        Process process = pb.start();
+        if (process.waitFor() != 0)
+            return;
+
+        // 2. Read params
+        List<Float> params = new ArrayList<Float>();
+        try {
+            File file = new File("params.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextFloat()) {
+                params.add(scanner.nextFloat());
+            }
+            file.delete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 3. Convert params to byte array and write the data to znode with the same
+        // workerId
+        byte[] vector = new byte[params.size() * 4];
+        for (int i = 0; i < params.size(); i++) {
+            byte[] byteRep = ByteBuffer.allocate(4).putFloat(params.get(i)).array();
+            for (int j = 0; j < 4; j++)
+                vector[4 * i + j] = byteRep[j];
+        }
+        zk.setData("/w" + workerId, vector, -1);
+    }
+
+    // 4. Get the average gradient from the manager znode
+    public void processResult(int rc, String path, Object ctx, byte[] data, Stat stat) {
+        if (rc != Code.OK.intValue()) {
+            System.out.println("Something's wrong");
+            return;
+        }
+
+        this.vector = new float[data.length / 4];
+        for (int j = 0; j < this.vector.length; j++)
+            this.vector[j] = ByteBuffer.wrap(data, 4 * j, 4).getFloat();
     }
 }
